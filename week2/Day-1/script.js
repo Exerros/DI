5ab9d51c792172c5af222be8ddb512ae816d9eb3
Daniel Sirot <danielsirot@gmail.com>
2023-01-23 22:28:23 +0200

    first commit

---
+++ week2/Day-1/script.js
@@ -0,0 +1,12 @@
+// primitives/value types
+// let name= 'mosh'; //string
+// let age= 30 ; // number
+// let isApproved = true; //boolean
+// let firstName = undefined; // undefined
+// let selectedColor = nulll; // null
+let person = {
+    name: 'Mosh', 
+    age: 30
+};
+
+console.log(person);
---
+++ week2/Day-2/exercises/script.js
@@ -0,0 +1,3 @@
+const sentence = "The movie is not that bad, I like it";
+const wordNot= se
+console.log(sentence)
