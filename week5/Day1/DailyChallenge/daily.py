4f4cdda23d7139152843dc24f6938322066128d9
Daniel Sirot <danielsirot@gmail.com>
2023-02-13 10:41:06 +0200

    missed the second part of the daily

--- week5/Day1/DailyChallenge/daily.py
+++ week5/Day1/DailyChallenge/daily.py
@@ -27,16 +27,27 @@ class Farm:
         result = f"{self.name}'s farm \n \n"
         for animal, amount in self.animals.items():
             result += f"{animal} : {amount}\n"
-        result += "\n E-I-E-I-0!"
+        result += "\n    E-I-E-I-0!"
         return result
+    
+    def get_animal_types(self):
+        animals_name= list(self.animals.keys())
+        animals_name.sort()
+        return animals_name
+    
+    def get_short_info(self):
+        animals=' '.join(self.get_animal_types())
+        return (f"{self.name}'s farm has {animals} ")
             
         
 danny=Farm("Danny")
 danny.add_animal("dog" , 12)
 danny.add_animal("dog" , 3)
 danny.add_animal("wolf" , 2)
-danny.add_animal("bear" , 1)
+danny.add_animal("bear")
 danny.add_animal("dragon" , 3)
 danny.add_animal("dragon" , 2)
 
 print(danny.get_info())
+print(danny.get_animal_types())
+print(danny.get_short_info())
