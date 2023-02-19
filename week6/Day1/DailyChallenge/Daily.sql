ef7ff5862421963eeaa340f0de3cda37bc3c4976
Daniel Sirot <danielsirot@gmail.com>
2023-02-19 21:27:42 +0200

    daily

---
+++ week6/Day1/DailyChallenge/Daily.sql
@@ -0,0 +1,3 @@
+-- select count (first_name) from actors
+insert into actors (first_name, last_name, age, number_oscars)
+-- VALUES('George', '','06/05/1961', ); --> error, can't add with blank fields, only using Null
