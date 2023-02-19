f4bf127361b213c924617d464df0ba77e453fd23
Daniel Sirot <danielsirot@gmail.com>
2023-02-19 20:30:51 +0200

    xp+

---
+++ week6/Day1/ExercisesXP/ExerciseXP+.sql
@@ -0,0 +1,33 @@
+-- create table students(
+-- 	id int primary key,
+-- 	last_name varchar(100),
+-- 	first_name varchar(100),
+-- 	birth_date date
+-- )
+
+-- insert into students 
+-- values 
+-- 	(1,'Benichou', 'Marc', '02/11/1998'),
+-- 	(2,'Cohen', 'Yoan', '03/12/2010'),
+-- 	(3,'Benichou', 'Lea', '27/07/1987'),
+-- 	(4,'Dux', 'Amelia', '07/04/1996'),
+-- 	(5,'Grez', 'David', '14/06/2003'),
+-- 	(6,'Simpson', 'Omer', '03/10/1980')
+-- ;
+
+--  select * from students;
+--  select (first_name, last_name) from students;
+-- 	select * from students where id =2;
+-- select * from students where last_name = 'Benichou' and first_name = 'Marc';
+-- select * from students where last_name = 'Benichou' or first_name = 'Marc';
+-- select * from students where first_name like '%a%';
+-- select * from students where first_name like 'a%';
+-- select * from students where first_name like '%a';
+-- select * from students where first_name like '%a_';
+-- select * from students where id IN (1, 3);
+-- select * from students where birth_date >= '01/01/2000'
+
+
+
+
+
