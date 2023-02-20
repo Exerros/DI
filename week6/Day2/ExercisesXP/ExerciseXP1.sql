b4470fdfb9b3d33a7f19c2c580be02faea29d6e8
Daniel Sirot <danielsirot@gmail.com>
2023-02-20 21:58:27 +0200

    w6d2

---
+++ week6/Day2/ExercisesXP/ExerciseXP1.sql
@@ -0,0 +1,15 @@
+-- Exercise 1 : Items And Customers
+
+-- SELECT * FROM items
+-- order by price;
+
+-- SELECT * FROM items where price >=80
+-- order by price desc
+
+-- select first_name, last_name from customers 
+-- order by first_name
+
+-- select last_name from customers
+-- order by last_name desc
+
+
---
+++ week6/Day2/ExercisesXP/ExerciseXP2.sql
@@ -0,0 +1,24 @@
+-- select * from customer
+
+-- select first_name, last_name as full_name from customer
+
+-- select distinct create_date from customer
+
+-- select * from customer
+-- order by first_name DESC;
+
+
+-- select film_ID, title, description, release_year, rental_rate from film
+-- order by rental_rate
+
+-- select address, phone from address where district = 'Texas'
+
+-- select * from film where film_id =15 or film_id=150
+
+-- select  film_ID, title, description, length, rental_rate from film where title = 'fight club'
+
+-- select  film_ID, title, description, length, rental_rate from film where title like 'fi________%'
+
+
+
+
